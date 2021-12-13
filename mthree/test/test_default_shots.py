20c2ac27edce978f0a7871c9f284c93d65558f82
Paul Nation <nonhermitian@gmail.com>
2021-12-13 10:09:07 -0500

    fix tests

--- mthree/test/test_default_shots.py
+++ mthree/test/test_default_shots.py
@@ -23,42 +23,18 @@ HIGH_SHOTS = 100000
 def test_athens_mod_shots1():
     """Check that default shots works properly for low settings"""
     backend = FakeAthens()
-
-    qc = QuantumCircuit(5)
-    qc.h(2)
-    qc.cx(2, 1)
-    qc.cx(2, 3)
-    qc.cx(1, 0)
-    qc.cx(3, 4)
-    qc.measure_all()
-
     backend._configuration.max_shots = LOW_SHOTS
-
-    raw_counts = execute(qc, backend).result().get_counts()
     mit = mthree.M3Mitigation(backend)
     mit.cals_from_system()
-    mit_counts = mit.apply_correction(raw_counts, qubits=range(5))
 
-    assert mit_counts.shots == LOW_SHOTS
+    assert mit.cal_shots == LOW_SHOTS
 
 
 def test_athens_mod_shots2():
     """Check that default shots works properly for high settings"""
     backend = FakeAthens()
-
-    qc = QuantumCircuit(5)
-    qc.h(2)
-    qc.cx(2, 1)
-    qc.cx(2, 3)
-    qc.cx(1, 0)
-    qc.cx(3, 4)
-    qc.measure_all()
-
     backend._configuration.max_shots = HIGH_SHOTS
-
-    raw_counts = execute(qc, backend).result().get_counts()
     mit = mthree.M3Mitigation(backend)
     mit.cals_from_system()
-    mit_counts = mit.apply_correction(raw_counts, qubits=range(5))
 
-    assert mit_counts.shots == 10000
+    assert mit.cal_shots == 10000
