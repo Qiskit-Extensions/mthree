4392936cb1750ceff53ef74e4de479b4390d98f4
Paul Nation <nonhermitian@gmail.com>
2021-12-13 10:37:09 -0500

    len is faster

--- mthree/mitigation.py
+++ mthree/mitigation.py
@@ -399,7 +399,7 @@ class M3Mitigation():
         Raises:
             M3Error: Bitstring length does not match number of qubits given.
         """
-        if not any(counts):
+        if not len(counts):
             raise M3Error('Input counts is any empty dict.')
         given_list = False
         if isinstance(counts, (list, np.ndarray)):
