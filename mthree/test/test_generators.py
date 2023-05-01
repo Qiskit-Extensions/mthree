4c034621e1897f08365298c5d1eeae0ffc8a83c3
Paul Nation <nonhermitian@gmail.com>
2023-05-01 14:25:06 -0400

    add more tests

--- mthree/test/test_generators.py
+++ mthree/test/test_generators.py
@@ -17,7 +17,6 @@ from mthree.generators import IndependentGenerator, RandomGenerator
 
 def test_random1():
     """Test random generator setting num_arrays works"""
-
     gen1 = RandomGenerator(5, 1)
     assert len(gen1.all_calibration_arrays()) == 1
 
@@ -30,7 +29,6 @@ def test_random1():
 
 def test_random2():
     """Test random generator can be called twice"""
-
     gen = RandomGenerator(7, 7)
     out1 = [item for item in gen]
     out2 = [item for item in gen]
@@ -40,9 +38,18 @@ def test_random2():
         assert np.allclose(out1[kk], out2[kk])
 
 
+def test_random3():
+    """Test random generator seeding works"""
+    gen1 = RandomGenerator(7, 8, seed=12345)
+    gen2 = RandomGenerator(7, 8, seed=12345)
+    out1 = [item for item in gen1]
+    out2 = [item for item in gen2]
+    for kk in range(8):
+        assert np.allclose(out1[kk], out2[kk])
+
+
 def test_independent1():
     """Test independent generator setting num_arrays works"""
-
     gen1 = IndependentGenerator(5)
     assert len(gen1.all_calibration_arrays()) == 5
 
@@ -55,7 +62,6 @@ def test_independent1():
 
 def test_independent2():
     """Test independent generator can be called twice"""
-
     gen = IndependentGenerator(77)
     out1 = [item for item in gen]
     out2 = [item for item in gen]
@@ -63,3 +69,11 @@ def test_independent2():
     assert len(out2) == 77
     for kk in range(77):
         assert np.allclose(out1[kk], out2[kk])
+
+
+def test_independent3():
+    """Test independent generator returns correct bit-arrays"""
+    num_qubits = 5
+    for idx, arr in enumerate(IndependentGenerator(num_qubits)):
+        # Since 0th qubit is right-most bit
+        assert np.where(arr == 1)[0][0] == num_qubits-idx-1
