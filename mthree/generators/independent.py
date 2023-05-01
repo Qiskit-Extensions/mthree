60fe4ed78196a302abdfbac0e6f9ef4b3daed4fd
Paul Nation <nonhermitian@gmail.com>
2023-05-01 14:00:08 -0400

    Add bit-array generators

---
+++ mthree/generators/independent.py
@@ -0,0 +1,67 @@
+# This code is part of Mthree.
+#
+# (C) Copyright IBM 2023.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+"""Independent bit-array generator
+"""
+import numpy as np
+
+from mthree.exceptions import M3Error
+
+
+class IndependentGenerator:
+    def __init__(self, num_qubits):
+        """Generator of arrays corresponding to a single x-gate on
+        one qubit at a time.
+        
+        This is primarily for use with simulators.
+
+        Parameters:
+            num_qubits (int): Number of qubits
+
+        Attributes:
+            num_qubits (int): Number of qubits / length of arrays
+            length (int): Total number of generated arrays, default=None (infinite)
+            seed (int): Seed used for RNG
+        """
+        self.num_qubits = num_qubits
+        self.length = num_qubits
+        self._iter_index = 0
+
+    def __iter__(self):
+        self._iter_index = 0
+        return self
+
+    def __next__(self):
+        if self._iter_index < self.length:
+            out = np.zeros(self.num_qubits, dtype=np.uint8)
+            out[self.num_qubits-self._iter_index-1] = 1
+            self._iter_index += 1
+            return out
+        else:
+            raise StopIteration
+
+    def all_calibration_arrays(self):
+        """Return all calibration arrays from generator
+        
+        Returns:
+            list: All calibration arrays, if length <= 10,000
+            
+        Raises:
+            Exception: Generator length is > 10,000
+        """
+        if self.length > 10000:
+            raise M3Error('Can only return all arrays for <= 10,000 qubits')
+        out = []
+        for kk in range(self.num_qubits):
+            temp = np.zeros(self.num_qubits, dtype=np.uint8)
+            temp[self.num_qubits-kk-1] = 1
+            out.append(temp)
+        return out
---
+++ mthree/test/test_generators.py
@@ -0,0 +1,65 @@
+# This code is part of Mthree.
+#
+# (C) Copyright IBM 2023.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+"""Test bit-array generators"""
+import numpy as np
+
+from mthree.generators import IndependentGenerator, RandomGenerator
+
+
+def test_random1():
+    """Test random generator setting num_arrays works"""
+
+    gen1 = RandomGenerator(5, 1)
+    assert len(gen1.all_calibration_arrays()) == 1
+
+    gen127 = RandomGenerator(50, 127)
+    assert len(gen127.all_calibration_arrays()) == 127
+
+    gen433 = RandomGenerator(433, 433)
+    assert len(gen433.all_calibration_arrays()) == 433
+
+
+def test_random2():
+    """Test random generator can be called twice"""
+
+    gen = RandomGenerator(7, 7)
+    out1 = [item for item in gen]
+    out2 = [item for item in gen]
+    assert len(out1) == 7
+    assert len(out2) == 7
+    for kk in range(7):
+        assert np.allclose(out1[kk], out2[kk])
+
+
+def test_independent1():
+    """Test independent generator setting num_arrays works"""
+
+    gen1 = IndependentGenerator(5)
+    assert len(gen1.all_calibration_arrays()) == 5
+
+    gen50 = IndependentGenerator(50)
+    assert len(gen50.all_calibration_arrays()) == 50
+
+    gen433 = IndependentGenerator(433)
+    assert len(gen433.all_calibration_arrays()) == 433
+
+
+def test_independent2():
+    """Test independent generator can be called twice"""
+
+    gen = IndependentGenerator(77)
+    out1 = [item for item in gen]
+    out2 = [item for item in gen]
+    assert len(out1) == 77
+    assert len(out2) == 77
+    for kk in range(77):
+        assert np.allclose(out1[kk], out2[kk])
