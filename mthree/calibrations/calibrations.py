7c4821e494a19ea39c0e2e44aa017f1157e56e3c
Paul Nation <nonhermitian@gmail.com>
2023-05-03 13:55:05 -0400

    Add calibrations object

---
+++ mthree/calibrations/calibrations.py
@@ -0,0 +1,131 @@
+# This code is part of Mthree.
+#
+# (C) Copyright IBM 2023.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+"""Calibration object"""
+import threading
+import warnings
+
+from qiskit import QuantumCircuit
+
+from mthree._helpers import system_info
+from .mapping import calibration_mapping
+
+
+class Calibration:
+    """Calibration object
+    """
+    def __init__(self, backend, qubits=None, generator=None):
+        """Calibration object
+
+        Parameters:
+            backend (Backend): Target backend
+            qubits (array_like): Physical qubits to calibrate over
+            generator (Generator): Generator of calibration circuits
+        """
+        if len(qubits) != generator.num_qubits:
+            raise Exception('Number of calibration qubits does '
+                            'not match generator.num_qubits')
+        
+        self.backend = backend
+        self.backend_info = system_info(backend)
+        self.generator = generator
+        self.bit_to_physical_mapping = calibration_mapping(self.backend,
+                                                           qubits=qubits)
+        self._calibration_data = None
+        self._thread = None
+        self._job_error = None
+        
+    def __getattribute__(self, attr):
+        """This allows for checking the status of the threaded cals call
+
+        For certain attr this will join the thread and/or raise an error.
+        """
+        __dict__ = super().__getattribute__("__dict__")
+        if attr in __dict__:
+            if attr in ["_calibration_data"]:
+                self._thread_check()
+        return super().__getattribute__(attr)
+    
+    def _thread_check(self):
+        """Check if a thread is running and join it.
+
+        Raise an error if one is given.
+        """
+        if self._thread and self._thread != threading.current_thread():
+            self._thread.join()
+            self._thread = None
+        if self._job_error:
+            raise self._job_error  # pylint: disable=raising-bad-type
+    
+    @property
+    def calibration_data(self):
+        if self._calibration_data is None:
+            raise Exception('Calibration is not calibrated')
+        return self._calibration_data
+
+    @calibration_data.setter
+    def calibration_data(self, cals):
+        if self._calibration_data is not None:
+            raise Exception('Calibration is already calibrated')
+        self._calibration_data = cals
+
+    def calibration_circuits(self):
+        """Calibration circuits from underlying generator
+        
+        Returns:
+            list: Calibration circuits
+        """
+        out_circuits = []
+        for string in self.generator:
+            qc = QuantumCircuit(self.backend_info['num_qubits'],
+                                self.generator.num_qubits)
+            for idx, val in enumerate(string[::-1]):
+                if val:
+                    qc.x(self.bit_to_physical_mapping[idx])
+                qc.measure(self.bit_to_physical_mapping[idx], idx)
+            out_circuits.append(qc)
+        return out_circuits
+
+    def calibrate_from_backend(self, shots=int(1e4), async_cal=True):
+        """Calibrate from the target backend using the generator circuits
+        
+        Parameters:
+            shots (int): Number of shots defining the precision of
+                         the underlying error elements
+        """
+        if self._calibration_data is not None:
+            warnings.warn('Calibration is already calibrated.  Overwriting')
+        self._calibration_data = None
+        cal_circuits = self.calibration_circuits()
+        self._job_error = None
+        cal_job = self.backend.run(cal_circuits, shots=shots)
+        if async_cal:
+            thread = threading.Thread(
+                target=_job_thread,
+                args=(cal_job, self),
+            )
+            self._thread = thread
+            self._thread.start()
+        else:
+            _job_thread(cal_job, self)
+
+        
+def _job_thread(job, cal):
+    """Grab job result async
+    """
+    try:
+        res = job.result()
+    # pylint: disable=broad-except
+    except Exception as error:
+        cal._job_error = error
+        return
+    else:
+        cal.calibration_data = res.get_counts()
---
+++ mthree/calibrations/mapping.py
@@ -0,0 +1,55 @@
+# This code is part of Mthree.
+#
+# (C) Copyright IBM 2023.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+"""Calibration bit-mapping"""
+import warnings
+
+from mthree._helpers import system_info
+
+
+def calibration_mapping(backend, qubits=None):
+    """Computes the bit to physical qubit mapping
+    for a calibration over a given set of qubits
+    
+    Parameters:
+        backend (Backend): A backend instance
+        qubits (list): Qubits to calibrate over, default=None is all
+        
+    Returns:
+        dict: Bit to physical qubit mapping
+    """
+    backend_info = system_info(backend)
+    if qubits is None:
+        qubits = range(backend_info['num_qubits'])
+        # Remove faulty qubits if any
+        if any(backend_info["inoperable_qubits"]):
+            qubits = list(
+                filter(
+                    lambda item: item not in backend_info["inoperable_qubits"],
+                    list(range(backend_info['num_qubits'])),
+                )
+            )
+            warnings.warn(
+                "Backend reporting inoperable qubits."
+                + " Skipping calibrations for: {}".format(
+                    backend_info["inoperable_qubits"]
+                )
+            )
+        num_qubits = len(qubits)
+        bit_to_phyical_mapping = {key: val for key,
+                                  val in zip(range(num_qubits), qubits)}
+    else:
+        if len(set(qubits)) != len(qubits):
+            raise Exception('Duplicate qubit indices')
+        num_qubits = len(qubits)
+        bit_to_phyical_mapping = {key:val for key,
+                                  val in zip(range(num_qubits), qubits)}
+    return bit_to_phyical_mapping
