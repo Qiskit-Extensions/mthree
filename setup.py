aaf002759fff94027a3b5991a503d74419672d02
Paul Nation <nonhermitian@gmail.com>
2024-09-23 09:22:06 -0400

    validate build is correct

--- setup.py
+++ setup.py
@@ -22,6 +22,9 @@ import setuptools
 import numpy as np
 from Cython.Build import cythonize
 
+if int(np.__version__.split('.')[0]) < 2:
+    raise Exception('Must build M3 against Numpy 2.0+')
+
 MAJOR = 2
 MINOR = 7
 MICRO = 0
