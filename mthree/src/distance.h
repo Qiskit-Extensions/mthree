b51a878a446feb86418e9beef89e871913f2c53c
Paul Nation <nonhermitian@gmail.com>
2024-09-27 13:04:01 -0400

    headers

--- mthree/src/distance.h
+++ mthree/src/distance.h
@@ -1,4 +1,16 @@
-/* Copyright (c) 2024 mthree */
+/*
+This code is part of Mthree.
+
+(C) Copyright IBM 2024.
+
+This code is licensed under the Apache License, Version 2.0. You may
+obtain a copy of this license in the LICENSE.txt file in the root directory
+of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+
+Any modifications or derivative works of this code must retain this
+copyright notice, and modified files need to carry a notice indicating
+that they have been altered from the originals.
+*/
 #include <stddef.h>
 #include <stdbool.h>
 
